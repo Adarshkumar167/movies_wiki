bc54b234e345e2b265dda1794ffc3293e8fa1c11
Adarsh <adarshkumar167@gmail.com>
2023-11-25 15:23:33 +0530

    NavBar_integrated

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -1,6 +1,22 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_html/flutter_html.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -33,13 +49,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
       body: ListView.builder(
         itemCount: _movies.length,
         itemBuilder: (context, index) {
+          final show = _movies[index]['show'];
+          final image = show['image'];
+          final summary = show['summary'] ?? '';
+          final truncatedSummary =
+              summary.split(' ').take(30).join(' ') + '...';
+
           return Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(vertical: 2),
             child: Container(
-              height: 200,
               decoration: const BoxDecoration(
                 color: Colors.black,
               ),
@@ -48,24 +72,157 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
                   children: [
                     SizedBox(
-                        height: 180,
-                        child: Image.network(
-                            _movies[index]['show']['image']['medium'])),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            _movies[index]['show']['name'],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                      height: 180,
+                      child: image != null && image['medium'] != null
+                          ? Image.network(image['medium'])
+                          : Image.network(
+                              'https://dummyimage.com/210x295/ffffff/000000.jpg&text=Unavailable',
                             ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              show['name'] ?? 'Unknown',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Html(
+                            data: truncatedSummary,
+                            style: {
+                              'body': Style(
+                                fontSize: FontSize(14.0),
+                                color: Colors.white,
+                              ),
+                            },
                           ),
                         ],
                       ),
-                    )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SearchScreen()),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  List _searchResults = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          decoration: const InputDecoration(
+            hintText: 'Search for a movie...',
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              searchMovies();
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: _searchResults.length,
+        itemBuilder: (context, index) {
+          final show = _searchResults[index]['show'];
+          final image = show['image'];
+          final summary = show['summary'] ?? '';
+          final truncatedSummary =
+              summary.split(' ').take(30).join(' ') + '...';
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 180,
+                      child: image != null && image['medium'] != null
+                          ? Image.network(image['medium'])
+                          : Image.network(
+                              'https://dummyimage.com/210x295/ffffff/000000.jpg&text=Unavailable',
+                            ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              show['name'] ?? 'Unknown',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Html(
+                            data: truncatedSummary,
+                            style: {
+                              'body': Style(
+                                fontSize: FontSize(14.0),
+                                color: Colors.white,
+                              ),
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -74,5 +231,19 @@ class _HomePageState extends State<HomePage> {
         },
       ),
     );
+  }
+
+  Future<void> searchMovies() async {
+    final searchTerm = _searchController.text;
+    final response = await http
+        .get(Uri.parse("https://api.tvmaze.com/search/shows?q=$searchTerm"));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _searchResults = jsonDecode(response.body);
+      });
+    } else {
+      throw Exception('Failed to search movies');
+    }
   }
 }
